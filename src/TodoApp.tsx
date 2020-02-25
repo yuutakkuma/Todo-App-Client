a43b78f432d116b1ddfe665521fa518a647e697e
yuuta <yuuta_cookie@icloud.com>
2020-02-26 02:31:02 +0900

    Header

---
+++ src/TodoApp.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Header } from './components/Header';
+
+export function TodoApp() {
+  return (
+    <div>
+      <Header />
+    </div>
+  );
+}
---
+++ src/components/Header.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export const Header: React.FC = () => {
+  return (
+    <header>
+      <div>
+        <h1>ToDo アプリ</h1>
+      </div>
+    </header>
+  );
+};
