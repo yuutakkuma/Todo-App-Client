0ab580623616161d8053c359768c2b4458315951
yuuta <yuuta_cookie@icloud.com>
2020-02-26 03:42:13 +0900

    stories

---
+++ src/components/CraateForm.stories.tsx
@@ -0,0 +1,43 @@
+import React, { useState } from 'react';
+
+import { CreateForm } from './CreateForm';
+import { Todo } from '../models/todo.model';
+import { List } from './List';
+
+export default {
+  title: 'TodoCreate'
+};
+
+export const Create = () => {
+  const [test, setTest] = useState<Todo[]>([]);
+
+  const todoAddHandler = (title: string) => {
+    setTest(prevItems => {
+      return [...prevItems, { title: title }];
+    });
+  };
+
+  const todoDeleteHandler = (id: number) => {
+    setTest(prevTodos => {
+      return prevTodos.filter((_value, index) => {
+        return index !== id;
+      });
+    });
+  };
+
+  return (
+    <div>
+      <CreateForm onAdd={todoAddHandler} />
+      {test.map((todoItems, index) => {
+        return (
+          <List
+            key={index}
+            id={index}
+            title={todoItems.title}
+            onDelete={todoDeleteHandler}
+          />
+        );
+      })}
+    </div>
+  );
+};
