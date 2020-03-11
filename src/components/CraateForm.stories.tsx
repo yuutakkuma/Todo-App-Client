d92654895d5e9fef845c1ea6e011a69486014987
yuuta <yuuta_cookie@icloud.com>
2020-03-11 22:46:25 +0900

    削除機能を追加

--- src/components/CraateForm.stories.tsx
+++ src/components/CraateForm.stories.tsx
@@ -1,43 +1,15 @@
-import React, { useState } from 'react';
+import React from 'react';
 
 import { CreateForm } from './CreateForm';
-import { Todo } from '../models/todo.model';
-import { List } from './List';
 
 export default {
   title: 'TodoCreate'
 };
 
 export const Create = () => {
-  const [test, setTest] = useState<Todo[]>([]);
-
-  const todoAddHandler = (title: string) => {
-    setTest(prevItems => {
-      return [...prevItems, { title: title }];
-    });
-  };
-
-  const todoDeleteHandler = (id: number) => {
-    setTest(prevTodos => {
-      return prevTodos.filter((_value, index) => {
-        return index !== id;
-      });
-    });
-  };
-
   return (
     <div>
-      <CreateForm onAdd={todoAddHandler} onTest={() => {}} />
-      {test.map((todoItems, index) => {
-        return (
-          <List
-            key={index}
-            id={index}
-            title={todoItems.title}
-            onDelete={todoDeleteHandler}
-          />
-        );
-      })}
+      <CreateForm />
     </div>
   );
 };
