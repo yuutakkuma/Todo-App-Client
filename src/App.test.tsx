7b8c619120e1256c787a998e87e6031f8f655138
yuuta <yuuta_cookie@icloud.com>
2020-03-21 16:27:04 +0900

    不要ファイルを削除

--- src/App.test.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import { TodoApp } from './components/TodoApp';
-
-test('renders learn react link', () => {
-  const { debug } = render(<TodoApp />);
-  debug();
-});
