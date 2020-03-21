309e05c6eefd02f82d5641cbae4f1013ef8ada62
yuuta <yuuta_cookie@icloud.com>
2020-03-21 16:26:10 +0900

    graphqlスキーマを追加

--- src/generated/graphql.tsx
+++ src/generated/graphql.tsx
@@ -4,79 +4,139 @@ import * as ApolloReactHooks from '@apollo/react-hooks';
 export type Maybe<T> = T | null;
 /** All built-in and custom scalars, mapped to their actual values */
 export type Scalars = {
-  ID: string;
-  String: string;
-  Boolean: boolean;
-  Int: number;
-  Float: number;
+  ID: string,
+  String: string,
+  Boolean: boolean,
+  Int: number,
+  Float: number,
 };
 
 export type CreateTodoInput = {
-  title: Scalars['String'];
+  title: Scalars['String'],
+};
+
+export type LoginInput = {
+  email: Scalars['String'],
+  password: Scalars['String'],
 };
 
 export type Mutation = {
-  __typename?: 'Mutation';
-  createTodo: Todo;
-  deleteTodo: Scalars['Boolean'];
+   __typename?: 'Mutation',
+  createTodo?: Maybe<Scalars['Boolean']>,
+  deleteTodo?: Maybe<Scalars['Boolean']>,
+  register?: Maybe<Scalars['Boolean']>,
+  login?: Maybe<Scalars['Boolean']>,
 };
+
 
 export type MutationCreateTodoArgs = {
-  input: CreateTodoInput;
+  input: CreateTodoInput
 };
 
+
 export type MutationDeleteTodoArgs = {
-  id: Scalars['ID'];
+  id: Scalars['ID']
+};
+
+
+export type MutationRegisterArgs = {
+  registerInput: RegisterInput
+};
+
+
+export type MutationLoginArgs = {
+  loginInput: LoginInput
 };
 
 export type Query = {
-  __typename?: 'Query';
-  getTodoList?: Maybe<Array<Maybe<Todo>>>;
+   __typename?: 'Query',
+  getTodoList?: Maybe<Array<Maybe<Todo>>>,
+  getUsers?: Maybe<Array<Maybe<User>>>,
+  bye?: Maybe<Scalars['String']>,
+};
+
+export type RegisterInput = {
+  userName: Scalars['String'],
+  email: Scalars['String'],
+  password: Scalars['String'],
 };
 
 export type Todo = {
-  __typename?: 'Todo';
-  id: Scalars['ID'];
-  title: Scalars['String'];
+   __typename?: 'Todo',
+  id: Scalars['ID'],
+  userId: Scalars['ID'],
+  title: Scalars['String'],
+};
+
+export type User = {
+   __typename?: 'User',
+  id: Scalars['ID'],
+  userName: Scalars['String'],
+  email: Scalars['String'],
+  todos?: Maybe<Array<Maybe<Todo>>>,
 };
 
 export type CreateTodoMutationVariables = {
-  title: Scalars['String'];
+  title: Scalars['String']
 };
 
-export type CreateTodoMutation = { __typename?: 'Mutation' } & {
-  createTodo: { __typename?: 'Todo' } & Pick<Todo, 'id' | 'title'>;
-};
+
+export type CreateTodoMutation = (
+  { __typename?: 'Mutation' }
+  & Pick<Mutation, 'createTodo'>
+);
 
 export type DeleteTodoMutationVariables = {
-  id: Scalars['ID'];
+  id: Scalars['ID']
 };
 
-export type DeleteTodoMutation = { __typename?: 'Mutation' } & Pick<
-  Mutation,
-  'deleteTodo'
->;
+
+export type DeleteTodoMutation = (
+  { __typename?: 'Mutation' }
+  & Pick<Mutation, 'deleteTodo'>
+);
 
 export type GetTodoListQueryVariables = {};
 
-export type GetTodoListQuery = { __typename?: 'Query' } & {
-  getTodoList: Maybe<
-    Array<Maybe<{ __typename?: 'Todo' } & Pick<Todo, 'id' | 'title'>>>
-  >;
+
+export type GetTodoListQuery = (
+  { __typename?: 'Query' }
+  & { getTodoList: Maybe<Array<Maybe<(
+    { __typename?: 'Todo' }
+    & Pick<Todo, 'id' | 'userId' | 'title'>
+  )>>> }
+);
+
+export type LoginMutationVariables = {
+  email: Scalars['String'],
+  password: Scalars['String']
 };
 
+
+export type LoginMutation = (
+  { __typename?: 'Mutation' }
+  & Pick<Mutation, 'login'>
+);
+
+export type RegisterMutationVariables = {
+  userName: Scalars['String'],
+  email: Scalars['String'],
+  password: Scalars['String']
+};
+
+
+export type RegisterMutation = (
+  { __typename?: 'Mutation' }
+  & Pick<Mutation, 'register'>
+);
+
+
 export const CreateTodoDocument = gql`
-  mutation CreateTodo($title: String!) {
-    createTodo(input: { title: $title }) {
-      id
-      title
-    }
-  }
-`;
-export type CreateTodoMutationFn = ApolloReactCommon.MutationFunction<
-  CreateTodoMutation,
-  CreateTodoMutationVariables
->;
+    mutation CreateTodo($title: String!) {
+  createTodo(input: {title: $title})
+}
+    `;
+export type CreateTodoMutationFn = ApolloReactCommon.MutationFunction<CreateTodoMutation, CreateTodoMutationVariables>;
 
 /**
  * __useCreateTodoMutation__
@@ -95,36 +155,18 @@ export type CreateTodoMutationFn = ApolloReactCommon.MutationFunction<
  *   },
  * });
  */
-export function useCreateTodoMutation(
-  baseOptions?: ApolloReactHooks.MutationHookOptions<
-    CreateTodoMutation,
-    CreateTodoMutationVariables
-  >
-) {
-  return ApolloReactHooks.useMutation<
-    CreateTodoMutation,
-    CreateTodoMutationVariables
-  >(CreateTodoDocument, baseOptions);
-}
-export type CreateTodoMutationHookResult = ReturnType<
-  typeof useCreateTodoMutation
->;
-export type CreateTodoMutationResult = ApolloReactCommon.MutationResult<
-  CreateTodoMutation
->;
-export type CreateTodoMutationOptions = ApolloReactCommon.BaseMutationOptions<
-  CreateTodoMutation,
-  CreateTodoMutationVariables
->;
+export function useCreateTodoMutation(baseOptions?: ApolloReactHooks.MutationHookOptions<CreateTodoMutation, CreateTodoMutationVariables>) {
+        return ApolloReactHooks.useMutation<CreateTodoMutation, CreateTodoMutationVariables>(CreateTodoDocument, baseOptions);
+      }
+export type CreateTodoMutationHookResult = ReturnType<typeof useCreateTodoMutation>;
+export type CreateTodoMutationResult = ApolloReactCommon.MutationResult<CreateTodoMutation>;
+export type CreateTodoMutationOptions = ApolloReactCommon.BaseMutationOptions<CreateTodoMutation, CreateTodoMutationVariables>;
 export const DeleteTodoDocument = gql`
-  mutation DeleteTodo($id: ID!) {
-    deleteTodo(id: $id)
-  }
-`;
-export type DeleteTodoMutationFn = ApolloReactCommon.MutationFunction<
-  DeleteTodoMutation,
-  DeleteTodoMutationVariables
->;
+    mutation DeleteTodo($id: ID!) {
+  deleteTodo(id: $id)
+}
+    `;
+export type DeleteTodoMutationFn = ApolloReactCommon.MutationFunction<DeleteTodoMutation, DeleteTodoMutationVariables>;
 
 /**
  * __useDeleteTodoMutation__
@@ -143,41 +185,27 @@ export type DeleteTodoMutationFn = ApolloReactCommon.MutationFunction<
  *   },
  * });
  */
-export function useDeleteTodoMutation(
-  baseOptions?: ApolloReactHooks.MutationHookOptions<
-    DeleteTodoMutation,
-    DeleteTodoMutationVariables
-  >
-) {
-  return ApolloReactHooks.useMutation<
-    DeleteTodoMutation,
-    DeleteTodoMutationVariables
-  >(DeleteTodoDocument, baseOptions);
-}
-export type DeleteTodoMutationHookResult = ReturnType<
-  typeof useDeleteTodoMutation
->;
-export type DeleteTodoMutationResult = ApolloReactCommon.MutationResult<
-  DeleteTodoMutation
->;
-export type DeleteTodoMutationOptions = ApolloReactCommon.BaseMutationOptions<
-  DeleteTodoMutation,
-  DeleteTodoMutationVariables
->;
+export function useDeleteTodoMutation(baseOptions?: ApolloReactHooks.MutationHookOptions<DeleteTodoMutation, DeleteTodoMutationVariables>) {
+        return ApolloReactHooks.useMutation<DeleteTodoMutation, DeleteTodoMutationVariables>(DeleteTodoDocument, baseOptions);
+      }
+export type DeleteTodoMutationHookResult = ReturnType<typeof useDeleteTodoMutation>;
+export type DeleteTodoMutationResult = ApolloReactCommon.MutationResult<DeleteTodoMutation>;
+export type DeleteTodoMutationOptions = ApolloReactCommon.BaseMutationOptions<DeleteTodoMutation, DeleteTodoMutationVariables>;
 export const GetTodoListDocument = gql`
-  query GetTodoList {
-    getTodoList {
-      id
-      title
-    }
+    query GetTodoList {
+  getTodoList {
+    id
+    userId
+    title
   }
-`;
+}
+    `;
 
 /**
  * __useGetTodoListQuery__
  *
  * To run a query within a React component, call `useGetTodoListQuery` and pass it any options that fit your needs.
- * When your component renders, `useGetTodoListQuery` returns an object from Apollo Client that contains loading, error, and data properties
+ * When your component renders, `useGetTodoListQuery` returns an object from Apollo Client that contains loading, error, and data properties 
  * you can use to render your UI.
  *
  * @param baseOptions options that will be passed into the query, supported options are listed on: https://www.apollographql.com/docs/react/api/react-hooks/#options;
@@ -188,33 +216,75 @@ export const GetTodoListDocument = gql`
  *   },
  * });
  */
-export function useGetTodoListQuery(
-  baseOptions?: ApolloReactHooks.QueryHookOptions<
-    GetTodoListQuery,
-    GetTodoListQueryVariables
-  >
-) {
-  return ApolloReactHooks.useQuery<GetTodoListQuery, GetTodoListQueryVariables>(
-    GetTodoListDocument,
-    baseOptions
-  );
-}
-export function useGetTodoListLazyQuery(
-  baseOptions?: ApolloReactHooks.LazyQueryHookOptions<
-    GetTodoListQuery,
-    GetTodoListQueryVariables
-  >
-) {
-  return ApolloReactHooks.useLazyQuery<
-    GetTodoListQuery,
-    GetTodoListQueryVariables
-  >(GetTodoListDocument, baseOptions);
-}
+export function useGetTodoListQuery(baseOptions?: ApolloReactHooks.QueryHookOptions<GetTodoListQuery, GetTodoListQueryVariables>) {
+        return ApolloReactHooks.useQuery<GetTodoListQuery, GetTodoListQueryVariables>(GetTodoListDocument, baseOptions);
+      }
+export function useGetTodoListLazyQuery(baseOptions?: ApolloReactHooks.LazyQueryHookOptions<GetTodoListQuery, GetTodoListQueryVariables>) {
+          return ApolloReactHooks.useLazyQuery<GetTodoListQuery, GetTodoListQueryVariables>(GetTodoListDocument, baseOptions);
+        }
 export type GetTodoListQueryHookResult = ReturnType<typeof useGetTodoListQuery>;
-export type GetTodoListLazyQueryHookResult = ReturnType<
-  typeof useGetTodoListLazyQuery
->;
-export type GetTodoListQueryResult = ApolloReactCommon.QueryResult<
-  GetTodoListQuery,
-  GetTodoListQueryVariables
->;
+export type GetTodoListLazyQueryHookResult = ReturnType<typeof useGetTodoListLazyQuery>;
+export type GetTodoListQueryResult = ApolloReactCommon.QueryResult<GetTodoListQuery, GetTodoListQueryVariables>;
+export const LoginDocument = gql`
+    mutation Login($email: String!, $password: String!) {
+  login(loginInput: {email: $email, password: $password})
+}
+    `;
+export type LoginMutationFn = ApolloReactCommon.MutationFunction<LoginMutation, LoginMutationVariables>;
+
+/**
+ * __useLoginMutation__
+ *
+ * To run a mutation, you first call `useLoginMutation` within a React component and pass it any options that fit your needs.
+ * When your component renders, `useLoginMutation` returns a tuple that includes:
+ * - A mutate function that you can call at any time to execute the mutation
+ * - An object with fields that represent the current status of the mutation's execution
+ *
+ * @param baseOptions options that will be passed into the mutation, supported options are listed on: https://www.apollographql.com/docs/react/api/react-hooks/#options-2;
+ *
+ * @example
+ * const [loginMutation, { data, loading, error }] = useLoginMutation({
+ *   variables: {
+ *      email: // value for 'email'
+ *      password: // value for 'password'
+ *   },
+ * });
+ */
+export function useLoginMutation(baseOptions?: ApolloReactHooks.MutationHookOptions<LoginMutation, LoginMutationVariables>) {
+        return ApolloReactHooks.useMutation<LoginMutation, LoginMutationVariables>(LoginDocument, baseOptions);
+      }
+export type LoginMutationHookResult = ReturnType<typeof useLoginMutation>;
+export type LoginMutationResult = ApolloReactCommon.MutationResult<LoginMutation>;
+export type LoginMutationOptions = ApolloReactCommon.BaseMutationOptions<LoginMutation, LoginMutationVariables>;
+export const RegisterDocument = gql`
+    mutation Register($userName: String!, $email: String!, $password: String!) {
+  register(registerInput: {userName: $userName, email: $email, password: $password})
+}
+    `;
+export type RegisterMutationFn = ApolloReactCommon.MutationFunction<RegisterMutation, RegisterMutationVariables>;
+
+/**
+ * __useRegisterMutation__
+ *
+ * To run a mutation, you first call `useRegisterMutation` within a React component and pass it any options that fit your needs.
+ * When your component renders, `useRegisterMutation` returns a tuple that includes:
+ * - A mutate function that you can call at any time to execute the mutation
+ * - An object with fields that represent the current status of the mutation's execution
+ *
+ * @param baseOptions options that will be passed into the mutation, supported options are listed on: https://www.apollographql.com/docs/react/api/react-hooks/#options-2;
+ *
+ * @example
+ * const [registerMutation, { data, loading, error }] = useRegisterMutation({
+ *   variables: {
+ *      userName: // value for 'userName'
+ *      email: // value for 'email'
+ *      password: // value for 'password'
+ *   },
+ * });
+ */
+export function useRegisterMutation(baseOptions?: ApolloReactHooks.MutationHookOptions<RegisterMutation, RegisterMutationVariables>) {
+        return ApolloReactHooks.useMutation<RegisterMutation, RegisterMutationVariables>(RegisterDocument, baseOptions);
+      }
+export type RegisterMutationHookResult = ReturnType<typeof useRegisterMutation>;
+export type RegisterMutationResult = ApolloReactCommon.MutationResult<RegisterMutation>;
+export type RegisterMutationOptions = ApolloReactCommon.BaseMutationOptions<RegisterMutation, RegisterMutationVariables>;
