db50712a423198c14b0c85dbb2d6e57aff692c13
yuuta <yuuta_cookie@icloud.com>
2020-03-25 03:08:58 +0900

    ログイン機能を実装

--- src/accessToken.ts
+++
@@ -1,9 +0,0 @@
-let accessToken: string;
-// レスポンスから受け取ったアクセストークンをセットする
-export const setAccessToken = (s: string) => {
-  accessToken = s;
-};
-// アクセストークンをゲットする
-export const getAccessToken = () => {
-  return accessToken;
-};
