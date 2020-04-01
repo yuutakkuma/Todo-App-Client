603e1b726f5942996e78ab6a8b5767c7a7b414e3
yuuta <yuuta_cookie@icloud.com>
2020-04-01 21:30:32 +0900

    DELETEボタンをコンポーネント化

--- src/components/List.tsx
+++ src/components/List.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { useDeleteTodoMutation } from '../generated/graphql';
+import { TodoDeleteButton } from './TodoDeleteButton';
 
 interface Props {
   id: string;
@@ -7,28 +7,10 @@ interface Props {
 }
 
 export const List: React.FC<Props> = props => {
-  const [deleteTodo, { error }] = useDeleteTodoMutation();
-
-  if (error) {
-    return <div>削除出来ませんでした。</div>;
-  }
-
   return (
     <div className="item-list">
       <p className="item-name">{props.title}</p>
-      <button
-        className="delete-btn"
-        data-testid="delete-btn-test"
-        onClick={() => {
-          deleteTodo({
-            variables: {
-              id: props.id
-            }
-          });
-        }}
-      >
-        DELETE
-      </button>
+      <TodoDeleteButton todoId={props.id} />
     </div>
   );
 };
