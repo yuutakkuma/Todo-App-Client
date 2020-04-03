db997040c255453a054d49c9f00fd0ca2af4c942
yuuta <yuuta_cookie@icloud.com>
2020-04-04 00:10:08 +0900

    Loading設定

--- src/components/button/RegisterButton.tsx
+++ src/components/button/RegisterButton.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
+import { Loading } from '../Loading';
 
 interface Props {
   isRegisterLoading: boolean;
 }
 
 export const RegisterButton: React.FC<Props> = props => {
-  if (props.isRegisterLoading) return <div>Loading...</div>;
+  if (props.isRegisterLoading) return <Loading />;
 
   return (
     <button className="auth-btn" type="submit">
--- src/components/button/TodoCreateButton.tsx
+++ src/components/button/TodoCreateButton.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
+import { Loading } from '../Loading';
 
 interface Props {
   isCreateLoading: boolean;
 }
 
 export const TodoCreateButton: React.FC<Props> = props => {
-  if (props.isCreateLoading) return <div>ロード中...</div>;
+  if (props.isCreateLoading) return <Loading />;
   return (
     <button className="todo-btn" type="submit">
       +
