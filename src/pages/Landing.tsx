10fbc7cfa9ad0c81be82708325f7797d8770c034
yuuta <yuuta_cookie@icloud.com>
2020-04-03 11:25:56 +0900

    ランディングページのデザイン変更

--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -1,14 +1,34 @@
 import React from 'react';
 import { Login } from './Login';
+import { useHistory } from 'react-router-dom';
 
 export const Landing: React.FC = () => {
+  const history = useHistory();
   return (
     <div className="main">
-      <div>
-        <h2>Todoアプリケーションへようこそ！！！</h2>
-        <p>ログイン、又は新規登録でアプリケーションを使用出来ます</p>
+      <div className="main-container">
+        <div className="box">
+          <h1 className="main-text">ToDoアプリケーションへようこそ！！！</h1>
+          <p className="main-text">
+            ログイン、又は新規登録でアプリケーションを使用出来ます
+          </p>
+        </div>
       </div>
-      <Login />
+      <div className="main-container">
+        <div className="box">
+          <Login />
+          <div className="box-inner">
+            <p className="main-text">又は</p>
+            <button
+              className="move-btn"
+              type="button"
+              onClick={() => history.push('/register')}
+            >
+              新規登録
+            </button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
