7cccb62bbec5e1a48e8f3851902df8f22a6d0d3e
yuuta <yuuta_cookie@icloud.com>
2020-04-04 00:40:22 +0900

    Login.tsxをcomponentsへ移動

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -2,9 +2,7 @@ import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import { TodoApp } from './pages/TodoApp';
 import { Register } from './pages/Register';
-import { Login } from './pages/Login';
 import { Landing } from './pages/Landing';
-import { Loading } from './components/Loading';
 
 export const Routes: React.FC = () => {
   return (
@@ -12,9 +10,7 @@ export const Routes: React.FC = () => {
       <Switch>
         <Route exact path="/" component={Landing} />
         <Route exact path="/todo" component={TodoApp} />
-        <Route exact path="/login" component={Login} />
         <Route exact path="/register" component={Register} />
-        <Route exact path="/loading" component={Loading} />
       </Switch>
     </BrowserRouter>
   );
