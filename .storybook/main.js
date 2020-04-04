4effa967be3adfc3174bc4c9d197ea7d06c8dadd
yuuta <yuuta_cookie@icloud.com>
2020-04-04 21:54:31 +0900

    storis作成、エラーで起動出来ない為、一旦保留。

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,8 +1,26 @@
+const path = require('path');
+
 module.exports = {
-  stories: ['../src/**/*.stories.tsx'],
+  stories: ['../stories/*.stories.tsx'],
   addons: [
     '@storybook/preset-create-react-app',
     '@storybook/addon-actions',
     '@storybook/addon-links'
-  ]
+  ],
+  webpackFinal: async config => {
+    config.module.rules.push({
+      test: /\.(ts|tsx)$/,
+      use: [
+        {
+          loader: require.resolve('ts-loader')
+        },
+        // Optional
+        {
+          loader: require.resolve('react-docgen-typescript-loader')
+        }
+      ]
+    });
+    config.resolve.extensions.push('.ts', '.tsx');
+    return config;
+  }
 };
