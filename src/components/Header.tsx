a940fa4f877547ce78dd9acafe211ba3621b63a1
yuuta <yuuta_cookie@icloud.com>
2020-04-06 11:10:57 +0900

    ヘッダーにアカウント削除ボタン設置

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,15 +2,28 @@ import React from 'react';
 import './componentStyle/Header.css';
 
 import { LogOutButton } from './button/LogOutButton';
+import { useHistory } from 'react-router-dom';
 
 export const Header: React.FC = () => {
+  const history = useHistory();
   return (
     <header>
-      <div>
-        <h1>ToDo アプリ</h1>
-      </div>
-      <div className="logout-box">
-        <LogOutButton />
+      <div className="header-container">
+        <div className="header-inner">
+          <h1>ToDo アプリ</h1>
+        </div>
+        <div className="header-btn-box">
+          <div className="header-btn-container">
+            <button
+              className="deleteaccount-move-btn"
+              type="button"
+              onClick={() => history.push('/')}
+            >
+              アカウント削除
+            </button>
+            <LogOutButton />
+          </div>
+        </div>
       </div>
     </header>
   );
