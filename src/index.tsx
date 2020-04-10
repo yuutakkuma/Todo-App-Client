ac7b34c580720b8a41f076938262e3fd5a4fa57f
yuuta <yuuta_cookie@icloud.com>
2020-04-11 00:17:56 +0900

    開発環境、本番環境でURLを変更

--- src/index.tsx
+++ src/index.tsx
@@ -6,9 +6,23 @@ import { ApolloProvider } from '@apollo/react-hooks';
 import './styles.css';
 import { App } from './App';
 
-// http://localhost:4000/graphql
+let graohqlUrl: string;
+
+// 開発用GraphQL URL
+if (process.env.NODE_ENV === 'development') {
+  graohqlUrl = process.env.REACT_APP_DEVELOPMENT_GRAPHQL_URL!;
+}
+// 本番環境用GraphQL URL
+if (process.env.NODE_ENV === 'production') {
+  graohqlUrl = process.env.REACT_APP_PRODUCTION_GRAPHQL_URL!;
+}
+// TypeGurde
+if (typeof graohqlUrl! === 'undefined') {
+  graohqlUrl = process.env.REACT_APP_DEVELOPMENT_GRAPHQL_URL!;
+}
+
 const client = new ApolloClient({
-  uri: 'https://blooming-atoll-62832.herokuapp.com/graphql',
+  uri: graohqlUrl,
   credentials: 'include'
 });
 
