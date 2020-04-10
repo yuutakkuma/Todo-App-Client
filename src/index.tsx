e9063482aa82c181c5b7ca8c0d3770e5d6c249bf
yuuta <yuuta_cookie@icloud.com>
2020-04-10 14:03:00 +0900

    url変更

--- src/index.tsx
+++ src/index.tsx
@@ -6,10 +6,9 @@ import { ApolloProvider } from '@apollo/react-hooks';
 import './styles.css';
 import { App } from './App';
 
+// http://localhost:4000/graphql
 const client = new ApolloClient({
-  uri:
-    'https://blooming-atoll-62832.herokuapp.com/graphql' ||
-    'http://localhost:4000/graphql',
+  uri: 'https://blooming-atoll-62832.herokuapp.com/graphql',
   credentials: 'include'
 });
 
