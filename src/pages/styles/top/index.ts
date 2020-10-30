c9a8b98767a625fef80b406f2b7a70bbc7ecc5ee
yuuta <watanabe@unlog.co.jp>
2020-10-30 12:10:26 +0900

    Add TopPage styles

---
+++ src/pages/styles/top/index.ts
@@ -0,0 +1,24 @@
+import styled from 'styled-components'
+
+export const StyledTopMain = styled.main`
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  flex-direction: row;
+  background-color: transparent;
+`
+
+export const StyledTopBox = styled.div`
+  width: 50%;
+  height: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+`
+
+export const StyledTopHeading = styled.h1`
+  color: rgba(253, 253, 253, 1);
+  font-size: 80px;
+  letter-spacing: 10px;
+`
