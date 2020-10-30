2d671c8646da17bd028f1264ec74a035da4cc3bb
yuuta <watanabe@unlog.co.jp>
2020-10-30 13:40:11 +0900

    Add className type

--- src/components/common/base/BaseInput/type.ts
+++ src/components/common/base/BaseInput/type.ts
@@ -3,6 +3,7 @@ import { ChangeEvent } from 'react'
 export interface Props {
   type?: string
   placeholder?: string
+  className?: string
   value: string | number
   onChange: (event: ChangeEvent<HTMLInputElement>) => void
 }
