09e6faa05aced8fc7204186b32c69f3a70f56868
yuuta <watanabe@unlog.co.jp>
2020-11-01 11:53:59 +0900

    Style adjustment

--- src/components/common/base/BaseButton/style.ts
+++ src/components/common/base/BaseButton/style.ts
@@ -3,7 +3,8 @@ import styled from 'styled-components'
 export const Button = styled.button`
   color: rgba(253, 253, 253, 1);
   background-color: rgba(50, 190, 230, 1);
-  border-color: rgba(0, 0, 0, 0.3);
+  border-color: rgba(244, 244, 242, 0.8);
+  border-style: solid;
   box-shadow: 1px 1px rgba(0, 0, 0, 0.3);
   outline: none;
   cursor: pointer;
